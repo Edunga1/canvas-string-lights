9113f89328d0f008771f594710e4da6ac8827917
edunga1 <goonr21@gmail.com>
2024-06-08 21:09:00 +0000

    Add click event, lights and etc

--- src/app.js
+++ src/app.js
@@ -17,12 +17,79 @@ function animate(callback) {
   })
 }
 
+function createDrawer(context) {
+  let info = {
+    width: 0,
+    height: 0,
+  }
+
+  const draw = (func) => {
+    context.save()
+    func(context, info)
+    context.restore()
+  }
+
+  const resize = (width, height) => {
+    info.width = width
+    info.height = height
+  }
+
+  resize()
+
+  return {
+    draw,
+    resize,
+  }
+}
+
+function background(context, info) {
+  context.fillStyle = 'ivory'
+  context.fillRect(0, 0, info.width, info.height)
+}
+
+function createLights() {
+  const elements = []
+
+  const lights = (context) => {
+    context.fillStyle = '000'
+    for (const { x, y } of elements) {
+      context.beginPath()
+      context.arc(x, y, 5, 0, Math.PI * 2)
+      context.fill()
+    }
+  }
+
+  const add = (x, y) => {
+    elements.push({ x, y })
+  }
+
+  return {
+    lights,
+    add,
+  }
+}
+
 function main() {
   const canvas = document.createElement('canvas')
+  const context = canvas.getContext('2d')
+  const { draw, resize } = createDrawer(context)
+  const { lights, add } = createLights(context)
+  const resizeFunc = () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    resize(window.innerWidth, window.innerHeight)
+  }
 
   document.body.appendChild(canvas)
+  window.addEventListener('resize', resizeFunc)
+  canvas.addEventListener('mousedown', e => {
+    add(e.clientX, e.clientY)
+  })
 
+  resizeFunc()
   animate(function () {
+    draw(background)
+    draw(lights)
   })
 }
 
