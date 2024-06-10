0eddf88d969ac42a300ba7c693e2c6123c68c1d9
edunga1 <goonr21@gmail.com>
2024-06-10 13:12:11 +0000

    Add animation for the string

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+//- drawers
+function drawLooseString(context, t, x1, y1, x2, y2, stringLength) {
+  const rightThickness = 10
+  const leftThickness = 10
+  const yDeltaScalar = 5
+  const yDelta = Math.abs(Math.sin(t / 10) * yDeltaScalar)
+  const deltaStringLength = stringLength + yDelta
+  context.beginPath()
+  context.moveTo(x1, y1)
+  context.bezierCurveTo(x1, y1 + deltaStringLength, x2, y2 + deltaStringLength, x2, y2)
+  context.lineTo(x2, y2 + rightThickness)
+  context.bezierCurveTo(x2, y2 + deltaStringLength, x1, y1 + deltaStringLength, x1, y1 + leftThickness)
+  context.closePath()
+  context.fill()
+}
+
 //- canvas elements
 function createLights() {
   const elements = []
@@ -8,17 +24,14 @@ function createLights() {
     context.fillStyle = '000'
     for (const element of elements) {
       // light
-      const { x, y, stringLength } = element
+      const { x, y, stringLength, life } = element
       context.beginPath()
       context.arc(x, y, 5, 0, Math.PI * 2)
       context.fill()
 
       // line
       if (last) {
-        context.beginPath()
-        context.moveTo(last.x, last.y)
-        context.bezierCurveTo(last.x, last.y + stringLength, x, y + stringLength, x, y)
-        context.stroke()
+        drawLooseString(context, life, last.x, last.y, x, y, stringLength)
       }
 
       // end
@@ -50,6 +63,11 @@ function createLights() {
     add,
     updater,
   }
+}
+
+function background(context, info) {
+  context.fillStyle = 'ivory'
+  context.fillRect(0, 0, info.width, info.height)
 }
 
 //- application
@@ -97,11 +115,6 @@ function createDrawer(context) {
   }
 }
 
-function background(context, info) {
-  context.fillStyle = 'ivory'
-  context.fillRect(0, 0, info.width, info.height)
-}
-
 function main() {
   const canvas = document.createElement('canvas')
   const context = canvas.getContext('2d')
@@ -112,6 +125,12 @@ function main() {
     canvas.height = window.innerHeight
     resize(window.innerWidth, window.innerHeight)
   }
+  add(100, 100)
+  add(200, 120)
+  add(300, 200)
+  add(500, 400)
+  add(700, 300)
+  add(1000, 600)
 
   document.body.appendChild(canvas)
   window.addEventListener('resize', resizeFunc)
