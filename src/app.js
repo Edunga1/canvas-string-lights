fe7a67bbab368245c7d42930bdcf829ba61ecd99
edunga1 <goonr21@gmail.com>
2024-06-19 14:45:15 +0000

    Add perspective to the lights
    closer to the edge, the light is bigger and sharper

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ function drawLooseString(context, t, x1, y1, x2, y2, stringLength, blurry = fals
   const yDelta = Math.abs(Math.cos(t / 5) * yDeltaScalar) * timeModifier
   const stringDepth = (stringLength + yDelta) * .9
 
-  context.fillStyle = blurry ? 'rgb(204, 204, 204)' : 'rgba(26, 8, 0, .4)'
+  context.fillStyle = blurry ? 'rgb(204, 204, 204)' : 'rgba(255, 204, 150, .2)'
   context.beginPath()
   context.moveTo(x1, y1)
   context.bezierCurveTo(x1, y1 + stringDepth, x2, y2 + stringDepth, x2, y2)
@@ -22,25 +22,35 @@ function drawLooseString(context, t, x1, y1, x2, y2, stringLength, blurry = fals
   context.fill()
 }
 
-function drawLight(context, x, y, border = false, blurry = false) {
-  function gradient(x, y, r, color) {
+function drawLight({
+  context,
+  x,
+  y,
+  z,
+  r,
+  border = false,
+  blurry = false,
+}) {
+  function circle(x, y, r, color) {
     const gradient = context.createRadialGradient(x, y, r / 20, x, y, r)
-    gradient.addColorStop(0, `rgba(${color}, .2)`)
-    gradient.addColorStop(1, `rgba(${color}, 0)`)
+    gradient.addColorStop(0, `rgba(${color}, 1)`)
+    gradient.addColorStop(1, `rgba(${color}, .75)`)
     context.fillStyle = gradient
+    context.filter = `blur(${z}px)`
     context.beginPath()
     context.arc(x, y, r, 0, Math.PI * 2)
     context.fill()
   }
+
   context.save()
 
   if (blurry === false) {
-    gradient(x, y, 100, '255, 224, 120')
+    circle(x, y, r, '255, 224, 120')
   }
 
   context.strokeStyle = border ? '#f00' : context.fillStyle
   context.lineWidth = 1
-  gradient(x, y, 5, blurry ? '204, 204, 204' : '255, 204, 150')
+  circle(x, y, 5, blurry ? '204, 204, 204' : '255, 204, 150')
   context.stroke()
 
   context.restore()
@@ -48,8 +58,13 @@ function drawLight(context, x, y, border = false, blurry = false) {
 
 //- canvas elements
 function createLights() {
-  let lightIndex = 0
   const searchRadius = 10
+  const maxZLevel = 5
+
+  let width = 0
+  let height = 0
+
+  let lightIndex = 0
   const elements = []
   const temporaryElements = []
   let lastElement = undefined
@@ -58,15 +73,31 @@ function createLights() {
     return lastElement ? distance(lastElement.x, lastElement.y, x, y) : 0
   }
 
+  function caculateZLevel(x, y) {
+    const toCenter = distance(width / 2, height * (3/4), x, y)
+    return (1 - toCenter / distance(0, 0, width / 2, height / 2)) * maxZLevel
+  }
+
+  function calculateRadius(z) {
+    return 10 + 40 * (1 - z / maxZLevel)
+  }
+
   function getElement(id) {
     return elements.find(e => e.id === id)
   }
 
-  function newElement(x, y) {
+  function newElement({
+    x,
+    y,
+    z = 0,
+    r = 5,
+  }) {
     return {
       id: lightIndex++,
       x,
       y,
+      z,  // distance to center. 0 ~ max z level, lower is closer to the edge
+      r,
       stringLength: distanceToLast(x, y),
       life: 0,
       highlighted: false,
@@ -81,13 +112,13 @@ function createLights() {
     let prev = undefined
 
     for (const element of elements) {
-      const { x, y, stringLength, life } = element
+      const { x, y, z, r, stringLength, life } = element
 
       if (prev) {
         drawLooseString(context, life, prev.x, prev.y, x, y, stringLength, false)
       }
 
-      drawLight(context, x, y, element.highlighted, false)
+      drawLight({context, x, y, z, r, border: element.highlighted, blurry: false})
 
       // end
       prev = element
@@ -100,18 +131,19 @@ function createLights() {
         drawLooseString(context, 0, prev.x, prev.y, x, y, stringLength, true)
       }
 
-      drawLight(context, x, y, false, true)
+      drawLight({context, x, y, z: 0, r: 5, border: false, blurry: true})
     }
   }
 
   const add = (x, y) => {
-    const element = newElement(x, y)
+    const z = caculateZLevel(x, y)
+    const element = newElement({x, y, z, r: calculateRadius(z)})
     elements.push(element)
     lastElement = element
   }
 
   const addTemporary = (x, y) => {
-    const element = newElement(x, y)
+    const element = newElement({x, y})
     temporaryElements.push(element)
   }
 
@@ -146,6 +178,11 @@ function createLights() {
     }
   }
 
+  const resize = (w, h) => {
+    width = w
+    height = h
+  }
+
   return {
     lights,
     add,
@@ -155,6 +192,7 @@ function createLights() {
     findByPosition,
     move,
     highlight,
+    resize,
   }
 }
 
@@ -223,18 +261,14 @@ function main() {
     findByPosition,
     move: moveLight,
     highlight,
-  } = createLights(context)
+    resize: resizeLights,
+  } = createLights()
   const resizeFunc = () => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     resize(window.innerWidth, window.innerHeight)
+    resizeLights(window.innerWidth, window.innerHeight)
   }
-  add(100, 100)
-  add(200, 120)
-  add(300, 200)
-  add(500, 400)
-  add(700, 300)
-  add(1000, 600)
 
   document.body.appendChild(canvas)
   window.addEventListener('resize', resizeFunc)
@@ -274,6 +308,14 @@ function main() {
     draw(background)
     draw(lights)
   })
+
+  // initial code
+  add(100, 100)
+  add(200, 120)
+  add(300, 200)
+  add(500, 400)
+  add(700, 300)
+  add(1000, 600)
 }
 
 main()
